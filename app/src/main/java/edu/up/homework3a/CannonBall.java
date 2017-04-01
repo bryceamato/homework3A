7e66e4213f113085fbc603beae4ee65defccc547
bryceamato <bryceamato89@gmail.com>
2017-03-31 23:19:18 -0700

    Initial commit

---
+++ app/src/main/java/edu/up/homework3a/CannonBall.java
@@ -0,0 +1,13 @@
+package edu.up.homework3a;
+
+/**
+ * Created by bryce on 3/31/2017.
+ */
+
+public class CannonBall
+{
+    private int speedX;
+    private int speedY;
+    private int posX;
+    private int posY;
+}
